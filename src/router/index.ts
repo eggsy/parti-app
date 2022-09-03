65ac2d8b362f4e102acf81f4ab3736f60eca2235
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-03 20:13:01 +0300

    feat: initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Components
+import Home from "@/views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-4xl">Hello World</div>
+</template>
