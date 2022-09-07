59fadbdcc4c53d9d31310c94dd60948006a9162f
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-07 03:35:21 +0300

    feat: initialize motion plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { MotionPlugin } from "@vueuse/motion";
+
 import "./styles/tailwind.css";
 
-createApp(App).mount("#app");
+createApp(App).use(MotionPlugin).mount("#app");
